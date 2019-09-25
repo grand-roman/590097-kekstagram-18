f89a1e176cc84103f1b79d99fc0f690071d27db5
Roman <31464179+grand-roman@users.noreply.github.com>
2019-09-25 22:02:57 +0700

    It's not all

--- js/main.js
+++ js/main.js
@@ -38,6 +38,7 @@ var permutationArray = function (array) {
 
 var urlsRandom = permutationArray(urls);
 
+//Случайный элемент с массива
 var getRandomElement = function (array) {
   var randomElement = Math.floor(Math.random() * array.length);
   return array[randomElement];
@@ -89,10 +90,10 @@ var picturesElement = document.querySelector('.pictures');
 picturesElement.appendChild(fragment);
 
 var bigPictureElement = document.querySelector('.big-picture');
-// Показываю элемент
-bigPictureElement.classList.remove('hidden');
+var bigPictureImage = bigPictureElement.querySelector('.big-picture__img img');
+
+
 // Заполняю данными из первого элемента массива pictures
-bigPictureElement.querySelector('.big-picture__img > img').src = pictures[0].url;
 bigPictureElement.querySelector('.likes-count').textContent = pictures[0].likes;
 bigPictureElement.querySelector('.comments-count').textContent = pictures[0].comments.length;
 bigPictureElement.querySelector('.social__caption').textContent = pictures[0].description;
@@ -113,3 +114,42 @@ var visuallyHidden = function (classString) {
 visuallyHidden('.social__comment-count');
 // и загрузку новых комментариев
 visuallyHidden('.social__comments-loader');
+
+var buttonBigPictureClose = document.body.querySelector('.big-picture__cancel');
+// Показываю оверлей
+var onPictureClick = function (evt) {
+  if (evt.target.classList.contains('picture__img')) {
+    bigPictureImage.src = evt.target.src;
+    bigPictureElement.classList.remove('hidden');
+    document.body.removeEventListener('click', onPictureClick);
+  }
+};
+
+// Открытие оверлея при клике на картинку
+document.body.addEventListener('click', onPictureClick);
+
+// Прячу оверлей
+var onButtonCloseClick = function () {
+  bigPictureElement.classList.add('hidden');
+  document.body.addEventListener('click', onPictureClick);
+};
+
+buttonBigPictureClose.addEventListener('click', onButtonCloseClick);
+
+// Загрузка изображения и показ формы для редактирования
+var uploadImageForm = document.querySelector('#upload-select-image');
+var uploadFileInput = picturesElement.querySelector('#upload-file');
+var imageUploadOverlay = picturesElement.querySelector('.img-upload__overlay');
+var buttonUploadClose = picturesElement.querySelector('.img-upload__cancel');
+
+// При выборе файла показываю форму редактирования
+uploadFileInput.addEventListener('change', function () {
+  imageUploadOverlay.classList.remove('hidden');
+  document.body.removeEventListener('click', onPictureClick);
+});
+// При нажатии на закрыть скрываю форму редактирования
+buttonUploadClose.addEventListener('click', function () {
+  imageUploadOverlay.classList.add('hidden');
+  uploadFileInput.value = '';
+  document.body.addEventListener('click', onPictureClick);
+});
